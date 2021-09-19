29b224f6f22eea21e424e4640f24bb3fca045aa1
Tyler Benton <me@tylerbenton.com>
2021-09-19 18:09:00 -0400

    Add `Project` component to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import Introduction from '../components/introduction';
+import Project from '../components/project';
 
 const Home = () => (
   <section>
     <div className="container">
-      <Introduction />
+      <Introduction className="home__introduction" />
+      <Project
+        url="https://github.com/tylerbenton0722/quizbit"
+        title="Quizbit"
+        tagline="Competitive Platform for Engineers of All Types"
+        technologies={['TypeScript', 'PostgreSQL', 'ReactJS', 'NodeJS', 'Redis', 'SequelizeJS']}
+      />
     </div>
   </section>
 );
